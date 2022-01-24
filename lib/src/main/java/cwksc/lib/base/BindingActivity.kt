cdb972f484cb971b3fd69d6d4914396218a2dcd5
CWKSC <53114952+CWKSC@users.noreply.github.com>
2022-01-25 00:32:01 +0800

    Update BindingActivity.kt

--- lib/src/main/java/cwksc/lib/base/BindingActivity.kt
+++ lib/src/main/java/cwksc/lib/base/BindingActivity.kt
@@ -6,23 +6,28 @@ import androidx.databinding.DataBindingUtil
 import androidx.viewbinding.ViewBinding
 
 /**
- * Add into build.gradle (Module) in root level but not in top
- * apply plugin: 'kotlin-kapt'
+ * Add into build.gradle (Module) inside plugins { }
+ *     id 'kotlin-kapt'
  *
- * Add into build.gradle (Module) inside android {}
+ * Add into build.gradle (Module) inside android { }
  * buildFeatures {
  *     dataBinding true
  * }
  */
+
+/**
+ * CustomBinding.onCreate() will run before onCreate(savedInstanceState: Bundle?)
+ */
 @Suppress("unused")
 open class BindingActivity<CustomBinding : ViewBinding>(private val layoutId: Int) : AppCompatActivity() {
 
+    @Suppress("MemberVisibilityCanBePrivate")
     lateinit var binding: CustomBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, layoutId)
         binding.onCreate()
+        super.onCreate(savedInstanceState)
     }
 
     open fun CustomBinding.onCreate() {}
