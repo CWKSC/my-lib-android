c824ee05acefe45a51ddef27959830429d511ca3
CWKSC <53114952+CWKSC@users.noreply.github.com>
2022-01-24 18:03:55 +0800

    Init module experiment

---
+++ experiment/src/main/java/cwksc/lib/experiment/framework/generalrecyclerviewadapter/ViewHolder.kt
@@ -0,0 +1,6 @@
+package cwksc.lib.experiment.framework.generalrecyclerviewadapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ViewHolder(view: View, var binding: Any) : RecyclerView.ViewHolder(view)
